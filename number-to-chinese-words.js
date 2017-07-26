7dce8d07adfb010ae5294eeea027b0250ba068cf
Colin Leung <colin@digi3studio.com>
2017-07-26 18:17:18 +0800

    update to export as UMD

--- number-to-chinese-words.js
+++ number-to-chinese-words.js
@@ -1,72 +1,115 @@
-'use strict';
+(function (global, factory) {
+  if (typeof define === "function" && define.amd) {
+    define(['exports'], factory);
+  } else if (typeof exports !== "undefined") {
+    factory(exports);
+  } else {
+    var mod = {
+      exports: {}
+    };
+    factory(mod.exports);
+    global.index = mod.exports;
+  }
+})(this, function (exports) {
+  'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var NumberToChineseWords = function () {
-  function NumberToChineseWords() {
-    _classCallCheck(this, NumberToChineseWords);
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
   }
 
-  _createClass(NumberToChineseWords, null, [{
-    key: 'toOrdinal',
-    value: function toOrdinal(num) {
-      return NumberToChineseWords.ordinal + num;
-    }
-  }, {
-    key: 'toWords',
-    value: function toWords(num) {
-      return NumberToChineseWords.intToChinese(num, NumberToChineseWords.digits, NumberToChineseWords.units) + NumberToChineseWords.floatToChinese(num, NumberToChineseWords.digits);
-    }
-  }, {
-    key: 'toWordsOrdinal',
-    value: function toWordsOrdinal(num) {
-      return NumberToChineseWords.ordinal + NumberToChineseWords.intToChinese(num, NumberToChineseWords.digits, NumberToChineseWords.units);
-    }
-  }, {
-    key: 'intToChinese',
-    value: function intToChinese(num, digits, units) {
-      var str = '';
-      var n = Math.floor(num);
-      while (n > 0) {
-        var u = units.shift();
-        var d = n % 10;
-        str = digits[d] + (d > 0 ? u : '') + str;
-        n = Math.floor(n / 10);
+  var _createClass = function () {
+    function defineProperties(target, props) {
+      for (var i = 0; i < props.length; i++) {
+        var descriptor = props[i];
+        descriptor.enumerable = descriptor.enumerable || false;
+        descriptor.configurable = true;
+        if ("value" in descriptor) descriptor.writable = true;
+        Object.defineProperty(target, descriptor.key, descriptor);
       }
-
-      return (num < 0 ? NumberToChineseWords.minus : "") + (num < 1 ? digits[0] : str.replace(new RegExp(NumberToChineseWords.digits[0] + "+$", "i"), '').replace(new RegExp("^" + NumberToChineseWords.digits[1] + NumberToChineseWords.units[1], "i"), NumberToChineseWords.units[1]));
     }
-  }, {
-    key: 'floatToChinese',
-    value: function floatToChinese(num, digits) {
-      if (num % 1 == 0) return;
 
-      var str = '';
-      var f = parseInt(num.toString().replace(/\d+./i, '1'));
-      while (f > 0) {
-        var d = f % 10;
-        str = digits[d] + str;
-        f = Math.floor(f / 10);
+    return function (Constructor, protoProps, staticProps) {
+      if (protoProps) defineProperties(Constructor.prototype, protoProps);
+      if (staticProps) defineProperties(Constructor, staticProps);
+      return Constructor;
+    };
+  }();
+
+  function intToChinese(num, digits, units, minus) {
+    var str = '';
+    var n = Math.floor(num);
+    var uc = units.slice();
+    while (n > 0) {
+      var u = uc.shift();
+      var d = n % 10;
+      str = digits[d] + (d > 0 ? u : '') + str;
+      n = Math.floor(n / 10);
+    }
+
+    var pattern1 = new RegExp(digits[0] + "+$", "i");
+    var pattern2 = new RegExp("^" + digits[1] + units[1], "i");
+    var pattern3 = new RegExp(digits[0] + "+", "i");
+
+    return (num < 0 ? minus : "") + (num < 1 ? digits[0] : str.replace(pattern1, '').replace(pattern2, units[1]).replace(pattern3, digits[0]));
+  }
+
+  function floatToChinese(num, digits, point) {
+    if (num % 1 == 0) return "";
+
+    var str = '';
+    var f = parseInt(num.toString().replace(/\d+./i, '1'));
+    while (f > 0) {
+      var d = f % 10;
+      str = digits[d] + str;
+      f = Math.floor(f / 10);
+    }
+
+    return point + str.replace(new RegExp("^" + digits[1], "i"), "");
+  }
+
+  var NC = function () {
+    function NC() {
+      _classCallCheck(this, NC);
+    }
+
+    _createClass(NC, null, [{
+      key: 'toOrdinal',
+      value: function toOrdinal(num) {
+        return NC.labels.ordinal + num;
       }
+    }, {
+      key: 'toWords',
+      value: function toWords(num) {
+        return intToChinese(num, NC.labels.digits, NC.labels.units, NC.labels.minus) + floatToChinese(num, NC.labels.digits, NC.labels.point);
+      }
+    }, {
+      key: 'toWordsOrdinal',
+      value: function toWordsOrdinal(num) {
+        return NC.labels.ordinal + intToChinese(num, NC.labels.digits, NC.labels.units, NC.labels.minus);
+      }
+    }]);
 
-      return NumberToChineseWords.point + str.replace(new RegExp("^" + NumberToChineseWords.digits[1], "i"), "");
-    }
-  }]);
+    return NC;
+  }();
 
-  return NumberToChineseWords;
-}();
-
-exports.default = NumberToChineseWords;
+  exports.default = NC;
 
 
-NumberToChineseWords.digits = ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九'];
-NumberToChineseWords.units = ['', '十', '百', '千', '萬', '十', '百', '千', '億', '十', '百', '千', '萬'];
-NumberToChineseWords.ordinal = "第";
-NumberToChineseWords.point = "點";
-NumberToChineseWords.minus = "負";
+  NC.labels = {
+    digits: ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九'],
+    units: ['', '十', '百', '千', '萬', '十', '百', '千', '億', '十', '百', '千', '萬'],
+    ordinal: "第",
+    point: "點",
+    minus: "負"
+  };
+
+  var toOrdinal = exports.toOrdinal = NC.toOrdinal;
+  var toWords = exports.toWords = NC.toWords;
+  var toWordsOrdinal = exports.toWordsOrdinal = NC.toWordsOrdinal;
+});
--- src/index.js
+++ src/index.js
@@ -1,46 +1,57 @@
-export default class NumberToChineseWords{
-  static toOrdinal(num){
-    return NumberToChineseWords.ordinal + num;
+function intToChinese(num, digits, units, minus){
+  let str = '';
+  let n = Math.floor(num);
+  let uc = units.slice();
+  while(n > 0){
+    let u = uc.shift();
+    let d = n % 10;
+    str = digits[d] + ((d > 0) ? u : '') + str;
+    n = Math.floor(n / 10);
   }
 
-  static toWords(num){
-    return NumberToChineseWords.intToChinese(num, NumberToChineseWords.digits, NumberToChineseWords.units) + NumberToChineseWords.floatToChinese(num, NumberToChineseWords.digits)
+  const pattern1 = new RegExp(digits[0]+"+$", "i");
+  const pattern2 = new RegExp("^"+digits[1] + units[1],"i");
+  const pattern3 = new RegExp(digits[0]+"+", "i");
+
+  return((num < 0) ? minus : "") + ((num < 1) ? digits[0] : str.replace(pattern1, '').replace(pattern2, units[1]).replace(pattern3, digits[0]));
+}
+
+function floatToChinese(num, digits, point){
+  if(num % 1 == 0)return "";
+
+  let str = '';
+  let f = parseInt(num.toString().replace(/\d+./i,'1'));
+  while(f>0){
+    let d = f%10;
+    str = digits[d] + str;
+    f = Math.floor(f/10);
   }
 
-  static toWordsOrdinal(num){
-    return NumberToChineseWords.ordinal + NumberToChineseWords.intToChinese(num, NumberToChineseWords.digits, NumberToChineseWords.units);
+  return point + str.replace(new RegExp("^" + digits[1],"i") , "");
+}
+
+export default class NC{
+  static toOrdinal(num) {
+    return NC.labels.ordinal + num;
   }
 
-  static intToChinese(num, digits, units){
-    let str = '';
-    let n = Math.floor(num);
-    while(n > 0){
-      let u = units.shift();
-      let d = n%10;
-      str = digits[d] + ((d > 0) ? u : '') + str;
-      n = Math.floor(n/10);
-    }
-
-    return((num<0)? NumberToChineseWords.minus : "") + ((num < 1)? digits[0] : str.replace(new RegExp(NumberToChineseWords.digits[0]+"+$", "i"), '').replace(new RegExp("^"+NumberToChineseWords.digits[1] + NumberToChineseWords.units[1],"i"), NumberToChineseWords.units[1]));
+  static toWords(num) {
+    return intToChinese(num, NC.labels.digits, NC.labels.units, NC.labels.minus) + floatToChinese(num, NC.labels.digits, NC.labels.point);
   }
 
-  static floatToChinese(num, digits){
-    if(num % 1 == 0)return;
-
-    let str = '';
-    let f = parseInt(num.toString().replace(/\d+./i,'1'));
-    while(f>0){
-      let d = f%10;
-      str = digits[d] + str;
-      f = Math.floor(f/10);
-    }
-
-    return NumberToChineseWords.point + str.replace(new RegExp("^" + NumberToChineseWords.digits[1],"i") , "");
+  static toWordsOrdinal(num) {
+    return NC.labels.ordinal + intToChinese(num, NC.labels.digits, NC.labels.units, NC.labels.minus);
   }
 }
 
-NumberToChineseWords.digits = ['零','一', '二', '三', '四', '五', '六', '七', '八', '九'];
-NumberToChineseWords.units = ['','十', '百', '千', '萬', '十', '百', '千', '億', '十', '百', '千', '萬'];
-NumberToChineseWords.ordinal = "第";
-NumberToChineseWords.point = "點";
-NumberToChineseWords.minus = "負";
+NC.labels = {
+  digits : ['零','一', '二', '三', '四', '五', '六', '七', '八', '九'],
+  units: ['','十', '百', '千', '萬', '十', '百', '千', '億', '十', '百', '千', '萬'],
+  ordinal: "第",
+  point: "點",
+  minus: "負"
+};
+
+export let toOrdinal = NC.toOrdinal;
+export let toWords = NC.toWords;
+export let toWordsOrdinal = NC.toWordsOrdinal;
